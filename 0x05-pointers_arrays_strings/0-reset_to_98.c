0ac10a403e86a39fe1a9dd6f3c5c527c7043dd05
AKAVICTOR <victoraka45@yahoo.com>
2023-09-18 10:06:05 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
+
 /**
- * reset_to_98 - this program resets the value of an int to 98
- *
- * @n: the value of a dereferencing pointer
- *
- * Return: nth
+ * reset_to_98 - resets n to 98
+ * @n: pointer to the value to be changed
+ * Return: void
  */
 void reset_to_98(int *n)
 {
